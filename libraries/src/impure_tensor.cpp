a5d2341398084a98d1e14488e2df548ed751ba74
Joy <kohchanman14@gmail.com>
2020-07-31 20:33:11 +0900

    nanの場合強制終了するコードを追加

--- libraries/src/impure_tensor.cpp
+++ libraries/src/impure_tensor.cpp
@@ -1,6 +1,7 @@
 #include "../include/impure_tensor.hpp"
 #include <cmath>
 #include <algorithm>
+#include <iostream>
 
 #define REP(i, N) for (int i = 0; i < (N); ++i)
 #define REP4(i, j, k, l, N) REP(i, N) REP(j, N) REP(k, N) REP(l, N)
@@ -59,68 +60,80 @@ int ImpureTensor::normalization(Tensor &T, ImpureTensor &originIMT, std::vector<
     int Dy = T.GetDy();
     bool isAllMerged = true;
     REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-        double t = std::abs(T(i, j, k, l));
-        if (t > 0) {
+                    double t = std::abs(T(i, j, k, l));
+                    if (std::isnan(t)) {
+                        std::cerr << "T(" << i << ',' << j << ',' << k << ',' << l << ") is nan";
+                        exit(1);
+                    }
+                    if (t > 0) {
 //            _min = std::min(_min, t);
-            _max = std::max(_max, t);
-        }
-    }
+                        _max = std::max(_max, t);
+                    }
+                }
     for (ImpureTensor &IMT : IMTs) {
         if (!IMT.isMerged) {
             isAllMerged = false;
             continue;
         }
-        for (Tensor &tensor : IMT.tensors) {
+        for (int n = 0; n < 3; ++n) {
             REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-                double t = std::abs(tensor(i, j, k, l));
-                if (t > 0) {
+                            double t = std::abs(IMT.tensors[n](i, j, k, l));
+                            if (std::isnan(t)) {
+                                std::cerr << "IMT[" << IMT.distance << "][" << n << "](" << i << ',' << j << ',' << k << ',' << l << ") is nan";
+                                exit(1);
+                            }
+                            if (t > 0) {
 //                    _min = std::min(_min, t);
-                    _max = std::max(_max, t);
-                }
-            }
+                                _max = std::max(_max, t);
+                            }
+                        }
         }
     }
     if (!isAllMerged) {
-        for (Tensor &tensor : originIMT.tensors) {
+        for (int n = 0; n < 3; ++n) {
             REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-                double t = std::abs(tensor(i, j, k, l));
-                if (t > 0) {
+                            double t = std::abs(originIMT.tensors[n](i, j, k, l));
+                            if (std::isnan(t)) {
+                                std::cerr << "originIMT[" << n << "](" << i << ',' << j << ',' << k << ',' << l << ") is nan";
+                                exit(1);
+                            }
+                            if (t > 0) {
 //                    _min = std::min(_min, t);
-                    _max = std::max(_max, t);
-                }
-            }
+                                _max = std::max(_max, t);
+                            }
+                        }
         }
     }
 //    auto o = static_cast<int>(std::floor((std::log10(_min) + std::log10(_max)) / 2));
     auto o = static_cast<int>(std::floor(std::log10(_max)));
     REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-        if (o > 0) {
-            REP(t, std::abs(o)) T(i, j, k, l) /= 10;
-        } else {
-            REP(t, std::abs(o)) T(i, j, k, l) *= 10;
-        }
-    }
+                    if (o > 0) {
+                        REP(t, std::abs(o)) T(i, j, k, l) /= 10;
+                    } else {
+                        REP(t, std::abs(o)) T(i, j, k, l) *= 10;
+                    }
+                }
     if (!isAllMerged) {
         for (Tensor &tensor : originIMT.tensors) {
             REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-                if (o > 0) {
-                    REP(t, std::abs(o)) tensor(i, j, k, l) /= 10;
-                } else {
-                    REP(t, std::abs(o)) tensor(i, j, k, l) *= 10;
-                }
-            }
+                            if (o > 0) {
+                                REP(t, std::abs(o)) tensor(i, j, k, l) /= 10;
+                            } else {
+                                REP(t, std::abs(o)) tensor(i, j, k, l) *= 10;
+                            }
+                        }
         }
     }
     for (ImpureTensor &IMT : IMTs) {
         if (!IMT.isMerged) continue;
         for (Tensor &tensor : IMT.tensors) {
             REP(i, Dx)REP(j, Dy)REP(k, Dx)REP(l, Dy) {
-                if (o > 0) {
-                    REP(t, std::abs(o)) tensor(i, j, k, l) /= 10;
-                } else {
-                    REP(t, std::abs(o)) tensor(i, j, k, l) *= 10;
-                }
-            }
+                            if (o > 0) {
+                                REP(t, std::abs(o)) tensor(i, j, k, l) /= 10;
+                            } else {
+                                REP(t, std::abs(o)) tensor(i, j, k, l) *= 10;
+                            }
+                        }
         }
     }
     return o;
