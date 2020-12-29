aa79cc60385f15a2fee26c04d3fcb96aa312f3fb
Joy <kohchanman14@gmail.com>
2020-12-29 14:51:01 +0900

    1点ずつ計算するよう修正

--- spherical_harmonics_HOTRG_2point_alt.cpp
+++ spherical_harmonics_HOTRG_2point_alt.cpp
@@ -17,8 +17,14 @@ using std::cout;
 using std::cerr;
 using std::string;
 
-void Trace(const int n_data_point, double const K, int const D_cut, int const l_max, int const N, std::ofstream &file) {
+void Trace(const int merge_point, double const K, int const D_cut, int const l_max, int const N, std::ofstream &file) {
     time_counter time;
+
+    /* distance */
+    long long int distance = 1;
+    REP(i, merge_point - 1) distance *= 2;
+    file << distance;
+
     // initialize tensor network : max index size is D_cut
     time.start();
     cout << "initialize tensor " << std::flush;
@@ -28,151 +34,135 @@ void Trace(const int n_data_point, double const K, int const D_cut, int const l_
     time.end();
     cout << "in " << time.duration_cast_to_string() << '\n' << std::flush;
 
-    std::vector<HOTRG::ImpureTensor> IMTs(n_data_point);
-
-    int Dx = D_cut, Dy = D_cut;
-
-    bool isMerged = false;
+    /* orders */
+    auto orders = new long long int[DIMENSION];
+    REP(i, DIMENSION) orders[i] = 0;
 
     for (int n = 1; n <= N; ++n) {
         time.start();
         cout << "N = " << std::setw(std::to_string(N).length()) << n << " :" << std::flush;
-
-        T.normalization(NORMALIZE_FACTOR);
-        for (auto &IMT : IMTs) {
-            if (IMT.isMerged) {
-                for (auto &tensor : IMT.tensors) tensor.normalization(NORMALIZE_FACTOR);
-            }
-        }
-        if (!isMerged) {
-            for (auto &tensor : originIMT.tensors) tensor.normalization(NORMALIZE_FACTOR);
-        }
+        const int times = (n + 1) / 2;
 
         if (n % 2) { // compress along x-axis
             cout << " compress along x-axis " << std::flush;
+            const int Dy = T.GetDy();
             auto U = new double[Dy * Dy * Dy * Dy];
             HOTRG::SVD_Y(D_cut, T, U);
-            const int times = (n + 1) / 2;
-            if (times <= n_data_point) {
-                int d = 1;
-                REP(i, times - 1) d *= 2;
-                IMTs[times - 1] = HOTRG::ImpureTensor(d, originIMT);
-                IMTs[times - 1].isMerged = true;
-                for (int i = 0; i < 3; ++i) {
-                    HOTRG::contractionX(D_cut, IMTs[times - 1].tensors[i], originIMT.tensors[i], U, "left");
+            if (times == merge_point) {
+                for (auto &tensor : originIMT.tensors) {
+                    HOTRG::contractionX(D_cut, tensor, tensor, U, "left");
                 }
-                for (int i = 0; i < times - 1; ++i) {
-                    for (auto &tensor : IMTs[i].tensors) HOTRG::contractionX(D_cut, tensor, T, U, "left");
-                }
+            } else {
                 for (auto &tensor : originIMT.tensors) {
                     HOTRG::contractionX(D_cut, tensor, T, U, "left");
-                }
-                if (times == n_data_point) isMerged = true;
-            } else {
-                for (auto &IMT : IMTs) {
-                    for (auto &tensor : IMT.tensors) HOTRG::contractionX(D_cut, tensor, T, U, "left");
                 }
             }
             HOTRG::contractionX(D_cut, T, T, U, "left");
             delete[] U;
         } else { // compress along y-axis
             cout << " compress along y-axis " << std::flush;
+            const int Dx = T.GetDx();
             auto U = new double[Dx * Dx * Dx * Dx];
             HOTRG::SVD_X(D_cut, T, U);
-            for (auto &IMT : IMTs) {
-                if (!IMT.isMerged) continue;
-                for (auto &tensor : IMT.tensors) HOTRG::contractionY(D_cut, tensor, T, U, "bottom");
-            }
-            if (!isMerged) {
-                for (auto &tensor : originIMT.tensors) HOTRG::contractionY(D_cut, tensor, T, U, "bottom");
-            }
+            for (auto &tensor : originIMT.tensors) HOTRG::contractionY(D_cut, tensor, T, U, "bottom");
             HOTRG::contractionY(D_cut, T, T, U, "bottom");
             delete[] U;
         }
 
-        Dx = T.GetDx();
-        Dy = T.GetDy();
-
-        if (n <= N / 2) {
-            time.end();
-            cout << "in " << time.duration_cast_to_string() << '\n';
-            continue;
+        /* normalization */
+        T.normalization(NORMALIZE_FACTOR);
+        for (auto &tensor : originIMT.tensors) tensor.normalization(NORMALIZE_FACTOR);
+        REP(i, DIMENSION) {
+            long long int order = originIMT.tensors[i].order - T.order;
+            if (times < merge_point) {
+                order *= 2;
+            }
+            orders[i] += order;
         }
+
+//        if (n <= N / 2) {
+//            time.end();
+//            cout << "in " << time.duration_cast_to_string() << '\n';
+//            continue;
+//        }
 
         double Tr = T.trace();
 
-        for (auto &IMT : IMTs) {
-            double impure_Tr[3];
-            REP(k, 3) {
-                impure_Tr[k] = IMT.tensors[k].trace();
-                int order = 0;
-                REP(i, T.orders.size()) {
-                    int m = IMT.tensors[k].orders[i] - T.orders[i];
-                    if (i < IMT.mergeIndex) m *= 2;
-                    order += m;
-                }
-                const int times = std::abs(order);
-                if (order > 0) {
-                    REP(i, times) impure_Tr[k] *= 10;
-                } else {
-                    REP(i, times) impure_Tr[k] /= 10;
-                }
+        double impureTrs[DIMENSION];
+        REP(i, DIMENSION) {
+            double impureTr = originIMT.tensors[i].trace();
+            const long long int order = orders[i];
+            const long long int absOrder = std::abs(order);
+            if (order > 0) {
+                REP(k, absOrder) impureTr *= NORMALIZE_FACTOR;
+            } else {
+                REP(k, absOrder) impureTr /= NORMALIZE_FACTOR;
             }
-            double res = (impure_Tr[0] - impure_Tr[1] + impure_Tr[2]) / Tr;
-            IMT.corrs.push_back(res);
-            cout << '\t' << std::scientific << std::setprecision(16) << res << std::flush;
+            impureTrs[i] = impureTr;
         }
+        double res = (impureTrs[0] - impureTrs[1] + impureTrs[2]) / Tr;
         time.end();
+        file << '\t' << std::scientific << std::setprecision(16) << res << std::flush;
+        cout << '\t' << std::scientific << std::setprecision(16) << res << std::flush;
         cout << "  in " << time.duration_cast_to_string() << '\n';
     }
-    for (auto &IMT : IMTs) {
-        file << IMT.distance;
-        for (double corr : IMT.corrs) {
-            file << '\t' << std::scientific << std::setprecision(16) << corr << std::flush;
-        }
-        file << '\n';
-    }
+    delete[] orders;
 }
 
 int main(int argc, char *argv[]) {
     /* inputs */
     int N = 40;     // volume : 2^N
-    int l_max;  // l_max
+    int l_max = 2;  // l_max
     int D_cut; // bond dimension
-    double K = 1.9; // inverse temperature
-    int n_data_point = 7; // number of d. d = 1, 2, 4, 8, 16, ...
+    double K = 1.80; // inverse temperature
+    int merge_point = 4; // d = 2^(merge_point - 1)
 
-    N = std::stoi(argv[1]);
-    l_max = std::stoi(argv[2]);
-    K = std::stod(argv[3]);
-    n_data_point = std::stoi(argv[4]);
+    if (argc == 5) {
+        N = std::stoi(argv[1]);
+        l_max = std::stoi(argv[2]);
+        K = std::stod(argv[3]);
+        merge_point = std::stoi(argv[4]);
+    }
 
     std::stringstream ss;
-    ss << std::fixed << std::setprecision(1) << K;
-    const string dir = "../data/spherical_harmonics/HOTRG_2point_alt/beta" + ss.str() + "/N" + std::to_string(N) + "/";
+    ss << std::fixed << std::setprecision(2) << K;
+    const string dir = "../data/spherical_harmonics/HOTRG_2point_alt/beta" + ss.str() + "/N" + std::to_string(N) + "/l" + std::to_string(l_max) + "/";
     time_counter time;
     string fileName;
     std::ofstream dataFile;
 
     /* calculation */
     time.start();
-    cout << "N = " << N << ", l_max = " << l_max << ", beta = " << K << ", n_data_point = " << n_data_point <<  '\n';
-    fileName = dir + "l" + std::to_string(l_max) + ".txt";
+    cout << "N = " << N << ", l_max = " << l_max << ", beta = " << ss.str() << ", merge_point = " << merge_point << '\n' << std::flush;
+    fileName = dir + std::to_string(merge_point) + ".txt";
     dataFile.open(fileName, std::ios::trunc);
     D_cut = (l_max + 1) * (l_max + 1);
-    Trace(n_data_point, K, D_cut, l_max, N, dataFile);
+    Trace(merge_point, K, D_cut, l_max, N, dataFile);
     dataFile.close();
     time.end();
     cout << "合計計算時間 : " << time.duration_cast_to_string() << '\n';
 
-    /* vs l_max*/
+    /* vs l_max */
 //    for (l_max = 4; l_max <= 6; ++l_max) {
 //        time.start();
 //        cout << "---------- " << l_max << " ----------\n" << std::flush;
-//        fileName = dir + "l" + std::to_string(l_max) + ".txt";
+//        fileName = dir + std::to_string(merge_point) + ".txt";
 //        dataFile.open(fileName, std::ios::trunc);
 //        D_cut = (l_max + 1) * (l_max + 1);
-//        Trace(n_data_point, K, D_cut, l_max, N, dataFile);
+//        Trace(merge_point, K, D_cut, l_max, N, dataFile);
+//        dataFile.close();
+//        time.end();
+//        cout << "合計計算時間 : " << time.duration_cast_to_string() << "\n\n";
+//    }
+
+    /* vs merge_point */
+//    for (merge_point = 1; merge_point <= 20; ++merge_point) {
+//        time.start();
+//        cout << "---------- " << merge_point << " ----------\n" << std::flush;
+//        fileName = dir + std::to_string(merge_point) + ".txt";
+//        dataFile.open(fileName, std::ios::trunc);
+//        D_cut = (l_max + 1) * (l_max + 1);
+//        Trace(merge_point, K, D_cut, l_max, N, dataFile);
 //        dataFile.close();
 //        time.end();
 //        cout << "合計計算時間 : " << time.duration_cast_to_string() << "\n\n";
