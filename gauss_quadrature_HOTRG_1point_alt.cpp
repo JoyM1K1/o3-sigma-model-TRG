aeae84dae555e47b57fe0dc6634661282c1d5bfb
Joy <kohchanman14@gmail.com>
2020-12-31 00:19:26 +0900

    出力を変更 / bug fix

--- gauss_quadrature_HOTRG_1point_alt.cpp
+++ gauss_quadrature_HOTRG_1point_alt.cpp
@@ -31,27 +31,28 @@ void Trace(double const K, int const D_cut, int const n_node, int const N, std::
     time.end();
     cout << "in " << time.duration_cast_to_string() << '\n' << std::flush;
 
+    /* orders */
+    long long int orders[DIMENSION];
+    for (auto & order : orders) order = 0;
 
-    int Dx = D, Dy = D;
+    /* correlations */
+    std::vector<double> correlations_list[DIMENSION];
 
     for (int n = 1; n <= N; ++n) {
         time.start();
         cout << "N = " << std::setw(std::to_string(N).length()) << n << " :" << std::flush;
 
-        T.normalization(NORMALIZE_FACTOR);
-        for (auto &tensor : originIMT.tensors) {
-            tensor.normalization(NORMALIZE_FACTOR);
-        }
-
         if (n % 2) { // compress along x-axis
-            cout << " compress along x-axis " << std::flush;
+            cout << " compress along x-axis : " << std::flush;
+            const int Dy = T.GetDy();
             auto U = new double[Dy * Dy * Dy * Dy];
             HOTRG::SVD_Y(D_cut, T, U);
             for (auto &tensor : originIMT.tensors) HOTRG::contractionX(D_cut, tensor, T, U, "left");
             HOTRG::contractionX(D_cut, T, T, U, "left");
             delete[] U;
         } else { // compress along y-axis
-            cout << " compress along y-axis " << std::flush;
+            cout << " compress along y-axis : " << std::flush;
+            const int Dx = T.GetDx();
             auto U = new double[Dx * Dx * Dx * Dx];
             HOTRG::SVD_X(D_cut, T, U);
             for (auto &tensor : originIMT.tensors) HOTRG::contractionY(D_cut, tensor, T, U, "bottom");
@@ -59,54 +60,60 @@ void Trace(double const K, int const D_cut, int const n_node, int const N, std::
             delete[] U;
         }
 
-        Dx = T.GetDx();
-        Dy = T.GetDy();
+        /* normalization */
+        T.normalization(NORMALIZE_FACTOR);
+        for (auto &tensor : originIMT.tensors) tensor.normalization(NORMALIZE_FACTOR);
+        REP(i, DIMENSION) orders[i] += originIMT.tensors[i].order - T.order;
 
         double Tr = T.trace();
 
-        double impure_Tr[DIMENSION];
-        REP(k, DIMENSION) {
-            long long int order = 0;
-            double tmp_Tr = originIMT.tensors[k].trace();
-            // TODO ここおかしい
-            REP(i, T.orders.size()) order += originIMT.tensors[k].orders[i] - T.orders[i];
-            const int times = std::abs(order);
+        double impureTrs[DIMENSION];
+        REP(i, DIMENSION) {
+            double impureTr = originIMT.tensors[i].trace();
+            const long long int order = orders[i];
+            const long long int absOrder = std::abs(order);
             if (order > 0) {
-                REP(i, times) tmp_Tr *= NORMALIZE_FACTOR;
+                REP(k, absOrder) impureTr *= NORMALIZE_FACTOR;
             } else {
-                REP(i, times) tmp_Tr /= NORMALIZE_FACTOR;
+                REP(k, absOrder) impureTr /= NORMALIZE_FACTOR;
             }
-            impure_Tr[k] = tmp_Tr;
+            impureTrs[i] = impureTr;
+            correlations_list[i].push_back(impureTr/Tr);
         }
         time.end();
-        file << std::scientific << std::setprecision(16) << impure_Tr[0] / Tr << '\t' << impure_Tr[1] / Tr << '\t' << impure_Tr[2] / Tr << '\n' << std::flush;
-        cout << std::scientific << std::setprecision(16) << impure_Tr[0] / Tr << '\t' << impure_Tr[1] / Tr << '\t' << impure_Tr[2] / Tr
+        cout << std::scientific << std::setprecision(16) << impureTrs[0] / Tr << '\t' << impureTrs[1] / Tr << '\t' << impureTrs[2] / Tr
              << "  in " << time.duration_cast_to_string() << '\n' << std::flush;
+    }
+    for (const auto& correlations : correlations_list) {
+        for (auto correlation : correlations) file << std::scientific << std::setprecision(16) << correlation << '\t';
+        file << '\n';
     }
 }
 
 int main(int argc, char *argv[]) {
     /* inputs */
-    int N = 10;     // volume : 2^N
+    int N = 40;     // volume : 2^N
     int n_node = 32;  // n_node
     int D_cut = 16; // bond dimension
-    double K = 1.8; // inverse temperature
+    double K = 1.80; // inverse temperature
 
-    N = std::stoi(argv[1]);
-    n_node = std::stoi(argv[2]);
-    D_cut = std::stoi(argv[3]);
-    K = std::stod(argv[4]);
+    if (argc == 5) {
+        N = std::stoi(argv[1]);
+        n_node = std::stoi(argv[2]);
+        D_cut = std::stoi(argv[3]);
+        K = std::stod(argv[4]);
+    }
 
     std::stringstream ss;
-    ss << std::fixed << std::setprecision(1) << K;
-    const string dir = "../data/gauss_quadrature/HOTRG_1point/beta" + ss.str() + "/N" + std::to_string(N) + "_node" + std::to_string(n_node) + "/";
+    ss << std::fixed << std::setprecision(2) << K;
+    const string dir = "../data/gauss_quadrature/HOTRG_1point_alt/beta" + ss.str() + "/N" + std::to_string(N) + "/node" + std::to_string(n_node) + "/";
     time_counter time;
     string fileName;
     std::ofstream dataFile;
 
     /* calculation */
     time.start();
-    cout << "N = " << N << ", node = " << n_node << ", D_cut = " << D_cut << ", beta = " << K << '\n';
+    cout << "N = " << N << ", node = " << n_node << ", D_cut = " << D_cut << ", beta = " << ss.str() << '\n' << std::flush;
     fileName = dir + "D" + std::to_string(D_cut) + ".txt";
     dataFile.open(fileName, std::ios::trunc);
     Trace(K, D_cut, n_node, N, dataFile);
