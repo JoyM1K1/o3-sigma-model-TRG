54adfd1052fbbc046ba90303bffaa54ac67e6ea7
Joy <kohchanman14@gmail.com>
2021-01-21 07:24:34 +0900

    set initial value to nullptr

--- libraries/include/tensor.hpp
+++ libraries/include/tensor.hpp
@@ -76,10 +76,10 @@ class tensor {
 private:
     int D[N]{};
     int D_prod[N]{};
-    int **indices{};
+    int **indices{nullptr};
     int array_size{0};
 public:
-    double *array;
+    double *array{nullptr};
 
     tensor();
 
@@ -99,8 +99,7 @@ public:
 
 template<int N>
 inline tensor<N>::tensor() {
-    array = new double[1];
-    array_size = 1;
+    array_size = 0;
     array[0] = 0;
     for (auto &d : D) d = 0;
     for (auto &d : D_prod) d = 0;
